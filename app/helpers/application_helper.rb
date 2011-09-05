ffe61a5386b3705c4a914c006a6a3b84eee09322
James Davis <james.davisphd@gmail.com>
2011-09-04 23:41:21 -0500

    Adding header and footer to the application along with some icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Micro Blog"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
+  def logo
+    image_tag("specializ.png", :alt => "Specializ", :class => "round")
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Specializ2::Application.routes.draw do
   resources :groupings
 
   resources :companies
+  
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
