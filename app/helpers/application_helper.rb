7a7c500068d8ec6dd39364c5059cc0a7fc50a900
James Davis <james.davisphd@gmail.com>
2011-09-05 14:43:50 -0500

    Added a draft of the topic browser.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # Return a title on a per-page basis.
   def title
-    base_title = "Micro Blog"
+    base_title = "Specializ"
     if @title.nil?
       base_title
     else
@@ -12,6 +12,19 @@ module ApplicationHelper
   
   def logo
     image_tag("specializ.png", :alt => "Specializ", :class => "round")
+  end
+  
+  def accordion(titles, expanders, options = {})
+   js = %Q(window.onload = function() {
+    var titles =
+     document.getElementsByClassName('#{titles}');
+    var expanders =
+     document.getElementsByClassName('#{expanders}');
+    var myEffect =
+     new Fx.Accordion(titles, expanders,
+       #{options_for_javascript(options)});
+   };)
+   javascript_tag(js.chop!)
   end
   
 end
