d0059405a5f4d6178e3d8eea48c560da8ce995ab
James Davis <james.davisphd@gmail.com>
2011-09-06 00:10:37 -0500

    Added groupings_link migration & table.

--- app/models/grouping.rb
+++ app/models/grouping.rb
@@ -2,4 +2,10 @@ class Grouping < ActiveRecord::Base
   belongs_to :company
   has_and_belongs_to_many :users, :uniq => true
   
+  has_and_belongs_to_many(:groupings,
+    :join_table => "groupings_link",
+    :foreign_key => "grouping_a_id",
+    :association_foreign_key => "grouping_b_id")
+
+  
 end
