02e2fbf209c53dc59c19b8282866f0aa6841580a
James Davis <james.davisphd@gmail.com>
2011-09-08 17:30:34 -0500

    Updating models as needed.  Adding has_many :through =>  to a couple models.

--- app/models/topic_group.rb
+++ app/models/topic_group.rb
@@ -1,7 +1,7 @@
 class TopicGroup < ActiveRecord::Base
   belongs_to :topic
   belongs_to :grouping
-  
+  has_many :assignments
   has_many :users, :through => :assignments
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,12 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :roles, :uniq => true
   has_and_belongs_to_many :groupings, :uniq => true
   belongs_to :company
-  
+  has_many :assignments
   has_many :topic_groups, :through => :assignments
   
+  def name
+    first_name.to_str + " " + last_name.to_str
+  end
   
   def role?(role)
     roles.include? role.to_string
