6c0e227eb26030955e60867c3f240a3b7712c99e
James Davis <james.davisphd@gmail.com>
2011-10-13 11:00:36 -0500

    Fixed the js that allowed navigating between topics.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,5 +9,14 @@ $(function() {
 	$( "#accordion" ).accordion();
 	$( "#manager_tabs" ).tabs();
 	$( "#hr_tabs" ).tabs();
-  
+	
+	$('#topic').change(function()
+		{
+			//alert($(this).attr('value'));
+			if ($(this).attr('value') > 0)
+				window.location.pathname = 'topics/'+$(this).attr('value')+'/edit';
+			else
+				window.location.pathname = 'topics/new';
+			
+		});
 });
