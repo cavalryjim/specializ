e08f398a5fe5a4a0d1f01a8de155d4f07f578e87
James Davis <james.davisphd@gmail.com>
2012-01-10 00:16:01 -0600

    Fixed some issues in the migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111220202552) do
+ActiveRecord::Schema.define(:version => 20120110052750) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -173,6 +173,7 @@ ActiveRecord::Schema.define(:version => 20111220202552) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "company_id"
+    t.string   "encrypted_password"
     t.string   "password_salt"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -182,7 +183,6 @@ ActiveRecord::Schema.define(:version => 20111220202552) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "encrypted_password"
     t.string   "encryptor"
     t.string   "pepper"
     t.string   "rpx_identifier"
