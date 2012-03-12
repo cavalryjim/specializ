ef03d75082a2a02a55e52cc590b08528ad7ba702
James Davis <james.davisphd@gmail.com>
2012-03-12 16:22:17 -0500

    Making the admin_user module more robust!

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,23 +1,17 @@
-ActiveAdmin.register User do
+ActiveAdmin.register AdminUser do
   index do
     column :id
-    column :first_name
-    column :last_name
     column :email
-    column :company
+    column :created_at 
     
     default_actions
   end
   
   form do |f|
     f.inputs do 
-      f.input :company
-      f.input :first_name
-      f.input :last_name
       f.input :email
       f.input :password
       f.input :password_confirmation
-      f.input :active
     end
     f.buttons
   end
--- app/admin/companies.rb
+++ app/admin/companies.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Company do
-  
+  index do
+    column :id
+    column :name
+    column :active
+    column :expiration_date
+    column :logo
+    
+    default_actions
+  end
 end
